78982b390f38c609b5f951457181f1d43cb1be52
Siser Pratap <siserinsevoc@gmail.com>
2024-06-24 18:48:40 +0530

    Added the paragraph tag

---
+++ todolist1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>I love Yashi</p>
+    </div>
+  );
+}
+
+export default App;
